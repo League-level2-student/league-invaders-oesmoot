4cec15072746eae4dd12bcfb97850f3652183e19
league <league@wts14-3.attlocal.net>
2024-10-26 11:30:17 -0700

    working on league invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,22 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	GamePanel gPanel;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	LeagueInvaders(){
+		this.frame = new JFrame();
+		this.gPanel = new GamePanel();
+	}
+public static void main(String[] args) {
+	LeagueInvaders invader = new LeagueInvaders();
+	invader.setup();
+}
+void setup() {
+	frame.add(gPanel);
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+}
 }
