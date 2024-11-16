dbd2571ddc2c4d7a5516a311f9ba07b643e8fb9a
league <karinabriellen@gmail.com>
2024-11-16 11:25:38 -0800

    working on objectmanager

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,6 @@
+import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class ObjectManager {
@@ -6,6 +8,8 @@ RocketShip rocket;
 ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 ArrayList<Alien> aliens = new ArrayList<Alien>();
 Random ran = new Random();
+Iterator<Alien> iterA = aliens.iterator();
+Iterator<Projectile> iterP = projectiles.iterator();
 ObjectManager(RocketShip rocket){
 	this.rocket = rocket;
 }
@@ -19,6 +23,35 @@ public void addAlien() {
 }
 
 public void update() {
-	
+	for(Alien a : aliens) {
+		a.update();
+		if(a.y>LeagueInvaders.HEIGHT) {
+			a.isActive = false;
+		}
+	}
+	for (Projectile p : projectiles) {
+		p.update();
+		if(p.y<0) {
+			p.isActive = false;
+		}
+	}
+}
+
+public void draw(Graphics g) {
+	rocket.draw(g);
+	for(Alien a: aliens) {
+		a.draw(g);
+	}
+	for (Projectile p: projectiles) {
+		p.draw(g);
+	}
+}
+public void purgeObjects() {
+	while(iterA.hasNext()) {
+		Alien a = iterA.next();
+		if(!a.isActive) {
+			iterA.remove();
+		}
+	}
 }
 }
